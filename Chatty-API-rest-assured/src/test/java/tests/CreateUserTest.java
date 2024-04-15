6e6de7d2d2b4fb67ee495ac7ab6065daa2e2dcc3
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2024-04-15 10:26:35 +0300

    Update CreateUserTest.java

--- Chatty-API-rest-assured/src/test/java/tests/CreateUserTest.java
+++ Chatty-API-rest-assured/src/test/java/tests/CreateUserTest.java
@@ -6,31 +6,57 @@ import dto.LoginResponseError;
 import dto.LoginResponseSuccess;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static tests.BaseTest.postRequest;
+import static tests.BaseTest.postRequestWithoutAccessToken;
 
 public class CreateUserTest {
     Faker faker = new Faker();// dlja random email/phone i t.d.
-    String fakeEmail = faker.internet().emailAddress();
-    String fakePassword = faker.regexify("[aK][io][l][t][u][0-9]{8,10}");
-    String confirmPassword =fakePassword;
-    String role = "user";
+    @Test
+    public void successCreateUserWithUserRole() { // v swagger 200 owibka, u menja 201
+        //Faker faker = new Faker();// dlja raddom email i t.d.
+        String userEmail = faker.internet().emailAddress();
+        String userPassword = faker.regexify("[a-zA-Z0-9]{8,10}");
+        String confirmPassword = userPassword;
+        String role = "user";
 
-//    @Test
-//    public void successCreateUser(){
-//       // CreateUserRequest requestBodyBuilder = CreateUserRequest.builder()
-//                .email(fakeEmail)
-//                .password(fakePassword)
-//                .confirmPassword(fakePassword)
-//                .role(role)
-//                .build();
-//
-//        LoginResponseSuccess user = postRequest("/auth/register",200, requestBodyBuilder)
-//                .body().jsonPath()
-//                .getObject("", LoginResponseSuccess.class);
-//        assertFalse(user.getExpiration().isEmpty());
-//        assertFalse(user.getAccessToken().isEmpty());
-//        assertFalse(user.getRefreshToken().isEmpty());
-//    }
+        //CreateUserRequest requestBody = new CreateUserRequest(userEmail, userPassword, confirmPassword,role );
+        CreateUserRequest reqBodyBuilder = CreateUserRequest.builder() // sozdajem
+                .email(userEmail)
+                .password(userPassword)
+                .confirmPassword(userPassword)
+                .role(role)
+                .build();
+        LoginResponseSuccess user = postRequest("/auth/register", 200,reqBodyBuilder) // sozdajem
+                .body().jsonPath()
+                .getObject("", LoginResponseSuccess.class);
+        //Check that all fields are not empty
+        assertFalse(user.getExpiration().isEmpty());
+        assertFalse(user.getAccessToken().isEmpty());
+        assertFalse(user.getRefreshToken().isEmpty());
+    }
+
+    @Test
+    public void createNewUserWithAdminRole() { // v swagger 401 owibka,a u menja 201
+        //Faker faker = new Faker();// dlja raddom email i t.d.
+        String userEmail = faker.internet().emailAddress();
+        String userPassword = faker.regexify("[a-zA-Z0-9]{8,10}");
+        String confirmPassword = userPassword;
+        String role = "admin";
+
+        //CreateUserRequest requestBody = new CreateUserRequest(userEmail, userPassword, confirmPassword,role );
+        CreateUserRequest reqBodyBuilder = CreateUserRequest.builder() // sozdajem
+                .email(userEmail)
+                .password(userPassword)
+                .confirmPassword(userPassword)
+                .role(role)
+                .build();
+        LoginResponseError userError = postRequestWithoutAccessToken("/auth/register", 401 ,reqBodyBuilder) // sozdajem
+                .body().jsonPath()
+                .getObject("", LoginResponseError.class);
+        //Check that all fields are not empty
+        assertEquals("Unauthorized", userError.getHttpStatus());
+    }
 
 }
