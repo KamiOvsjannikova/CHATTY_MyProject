42c276ed02f8cbd3588bf5f102d937efbf7a5eca
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2024-04-15 09:25:15 +0200

    Add files via upload

---
+++ Chatty-API-rest-assured/src/test/java/dto/UserUpdateRequest.java
@@ -0,0 +1,19 @@
+package dto;
+
+public class UserUpdateRequest {
+    private String avatarUrl;
+    private String name;
+    private String surname;
+    private String birthDate;
+    private String phone;
+    private String gender;
+    private String backgroundUrl;
+    //"blocked": true
+
+}
+
+
+
+
+
+
