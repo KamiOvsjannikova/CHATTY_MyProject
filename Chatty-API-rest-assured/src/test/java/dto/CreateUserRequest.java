30186bbce36a2b56b9a181e1c8e770da8996e0c5
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2024-04-18 23:34:34 +0300

    Update CreateUserRequest.java

--- Chatty-API-rest-assured/src/test/java/dto/CreateUserRequest.java
+++ Chatty-API-rest-assured/src/test/java/dto/CreateUserRequest.java
@@ -1,4 +1,16 @@
 package dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data@AllArgsConstructor@NoArgsConstructor@Builder
 public class CreateUserRequest {
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String role;
 }
