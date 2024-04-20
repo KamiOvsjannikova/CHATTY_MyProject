431b2b20b81320005aab2cf67d9c02a1892464df
KamiOvsjannikova <132999235+KamiOvsjannikova@users.noreply.github.com>
2024-04-20 17:08:21 +0300

    Update DraftPage.java

--- Chatty-selenide-Project/src/main/java/org/example/DraftPage.java
+++ Chatty-selenide-Project/src/main/java/org/example/DraftPage.java
@@ -1,13 +1,8 @@
 package org.example;
-
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import java.time.Duration;
-
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -19,23 +14,3 @@ public class DraftPage {
         post.filterBy(text(postTitle)).shouldHave(sizeGreaterThan(0));
     }
 }
-
-
-
-
-
-//    public SelenideElement getMessageTextDraftSaveSuccessfully() {
-//        return messageTextDraftSaveSuccessfully;
-//    }
-//    public ElementsCollection getPost() {
-//        return post;
-//    }
-//    public void inputSearchRequest(String Hallo){
-//
-//        post.texts();
-//    }
-//
-//    public void messageTextIsDisplayed() {
-//        getMessageTextDraftSaveSuccessfully().shouldHave(text("Draft save successfully"));
-//    }
-
